ac0acf5854f593ff7de27c3dd8f52657d801af7c
MatheusMarinho77 <matheusmoreiramarinho@outlook.com>
2025-06-07 19:06:18 -0300

    Testando GitLab

--- Sprint_1/Exercicio_16/Exercicio_16.py
+++ Sprint_1/Exercicio_16/Exercicio_16.py
@@ -1,7 +1,7 @@
 ## LER UM NUMERO REAL PELO TECLADO E MOSTRE A SUA POSIÇÃO INTEIRA
 
-import math
+from math import trunc
 
 numero = float(input('Informe um numero do conjunto dos numeros Reais:'))
 
-print('O numero {} tem a parte inteira {}'.format(numero, math.trunc(numero)))
+print('O numero {} tem a parte inteira {}'.format(numero, trunc(numero)))
---
+++ Sprint_1/Exercicio_16/Exercicio_20.py
@@ -0,0 +1,3 @@
+##sjefoesnfois
+
+
